f45cc2386e5382453ed664e4576d975d65b55bf5
Keturah Howard <keturah.dev@gmail.com>
2020-02-07 10:05:20 -0800

    create and fail test counting years lived past life expectency

--- __test__/age.test.js
+++ __test__/age.test.js
@@ -3,15 +3,22 @@ import { Age } from './../src/age.js';
 describe('Age', () => {
 
   const input = 21;
+  const olderInput = 83;
   let age;
+  let olderAge;
   beforeEach(() => {
-    age = new Age(input)
+    age = new Age(input);
+    olderAge = new Age(olderInput);
   })
   test('should return age of user', () => {
     expect(age.userAge).toEqual(21);
   })
   test('should return life expectency left on earth', () => {
-    age.lifeLeftOnEarth()
-    expect(age.remainingEarthLife).toEqual(52)
+    age.lifeLeftOnEarth();
+    expect(age.remainingEarthLife).toEqual(52);
+  })
+  test('should return amount of years lived past life expectancy', () => {
+    age.lifeLeftOnEarth();
+    expect(age.winningBy).toEqual(11)
   })
 })
