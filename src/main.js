5c4f63a94b7a77f802318fe991c2204c29b34f46
Keturah Howard <keturah.dev@gmail.com>
2020-02-07 15:06:58 -0800

    attempt to implement click functions to show info divs

--- src/main.js
+++ src/main.js
@@ -1,19 +1,41 @@
 import $ from 'jquery';
 import 'bootstrap';
-import './styles.css'
+import './styles.css';
 import { Age } from './age.js';
 
+
+function mercuryInfo(age) {
+  age.getMerked(age.userAge);
+  // $('#mercury').show();
+  $('#mercAge').html(age.newYears);
+}
+// function venusInfo(age) {
+//   age.getVenused(age.userAge);
+//   $('#venus').show();
+//   $('#venAge').html(age.newYears);
+// }
+// function marsInfo(age) {
+//   age.getMarsed(age.userAge);
+//   $('#mars').show();
+//   $('#marsAge').html(age.newYears);
+// }
+// function jupiterInfo(age) {
+//   age.getJupt(age.userAge);
+//   $('#jupiter').show();
+//   $('#juptAge').html(age.newYears);
+// }
 $(document).ready(function () {
   $('#formy').submit(function (event) {
     event.preventDefault();
     let age = $('#age').val();
     let usersAge = new Age(age);
 
+    $('.output').show();
     $('#currentAge').html(usersAge.userAge);
 
-
-    console.log(usersAge.userAge);
-    $('.output').show();
-    $('#mercAge').append(usersAge.userAge)
+    $('#mercButton').click(mercuryInfo(usersAge));
+    // $('#venusButton').click(venusInfo(usersAge));
+    // $('#marsButton').click(marsInfo(usersAge));
+    // $('#jupiterButton').click(jupiterInfo(usersAge));
   });
 });
