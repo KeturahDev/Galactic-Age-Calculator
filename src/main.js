fed3259875942b91e56688f222ee05fc4625b98c
Keturah Howard <keturah.dev@gmail.com>
2020-02-07 13:23:14 -0800

    import a few things, console log new age object

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
+import $ from 'jquery';
 import { Age } from './age.js';
-import $ from jquery;
+import 'bootstrap';
 
 $(document).ready(function () {
   $('#formy').submit(function (event) {
     event.preventDefault();
     let age = $('#age').val();
+    let usersAge = new Age(age);
 
-  })
-})
+    console.log(usersAge);
+  });
+});
