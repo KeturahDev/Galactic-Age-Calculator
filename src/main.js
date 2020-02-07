5921a6afca62eb6066c1fe91f1517f7e4c97084f
Keturah Howard <keturah.dev@gmail.com>
2020-02-07 15:24:19 -0800

    create functions for displaying age in years of corresponding planet buttons

--- src/main.js
+++ src/main.js
@@ -6,24 +6,36 @@ import { Age } from './age.js';
 
 function mercuryInfo(age) {
   age.getMerked(age.userAge);
-  // $('#mercury').show();
+  $('#mercury').toggle();
+  $('#mars').hide();
+  $('#jupiter').hide();
+  $('#venus').hide();
   $('#mercAge').html(age.newYears);
 }
-// function venusInfo(age) {
-//   age.getVenused(age.userAge);
-//   $('#venus').show();
-//   $('#venAge').html(age.newYears);
-// }
-// function marsInfo(age) {
-//   age.getMarsed(age.userAge);
-//   $('#mars').show();
-//   $('#marsAge').html(age.newYears);
-// }
-// function jupiterInfo(age) {
-//   age.getJupt(age.userAge);
-//   $('#jupiter').show();
-//   $('#juptAge').html(age.newYears);
-// }
+function venusInfo(age) {
+  age.getVenused(age.userAge);
+  $('#venus').toggle();
+  $('#mercury').hide();
+  $('#jupiter').hide();
+  $('#mars').hide();
+  $('#venAge').html(age.newYears);
+}
+function marsInfo(age) {
+  age.getMarsed(age.userAge);
+  $('#mars').toggle();
+  $('#mercury').hide();
+  $('#jupiter').hide();
+  $('#venus').hide();
+  $('#marsAge').html(age.newYears);
+}
+function jupiterInfo(age) {
+  age.getJupt(age.userAge);
+  $('#jupiter').toggle();
+  $('#mercury').hide();
+  $('#venus').hide();
+  $('#mars').hide();
+  $('#juptAge').html(age.newYears);
+}
 $(document).ready(function () {
   $('#formy').submit(function (event) {
     event.preventDefault();
@@ -33,9 +45,17 @@ $(document).ready(function () {
     $('.output').show();
     $('#currentAge').html(usersAge.userAge);
 
-    $('#mercButton').click(mercuryInfo(usersAge));
-    // $('#venusButton').click(venusInfo(usersAge));
-    // $('#marsButton').click(marsInfo(usersAge));
-    // $('#jupiterButton').click(jupiterInfo(usersAge));
+    $('#mercButton').click(function () {
+      mercuryInfo(usersAge);
+    });
+    $('#venusButton').click(function () {
+      venusInfo(usersAge);
+    });
+    $('#marsButton').click(function () {
+      marsInfo(usersAge);
+    });
+    $('#jupiterButton').click(function () {
+      jupiterInfo(usersAge);
+    });
   });
 });
