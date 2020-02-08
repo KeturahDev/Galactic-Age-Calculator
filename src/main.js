d4e9cf390a9d75e42bb2892e1f2880f3a9417be5
Keturah Howard <keturah.dev@gmail.com>
2020-02-07 16:31:15 -0800

    add ids to html, hide them in styles, and add functionality to display and input life expectency for years left per planet age

--- src/main.js
+++ src/main.js
@@ -5,36 +5,81 @@ import { Age } from './age.js';
 
 
 function mercuryInfo(age) {
-  age.getMerked(age.userAge);
-  $('#mercury').toggle();
+  $('#mercury').show();
   $('#mars').hide();
   $('#jupiter').hide();
   $('#venus').hide();
+  age.getMerked(age.userAge);
   $('#mercAge').html(age.newYears);
+  age.lifeLeftOnEarth();
+  if (age.userAge < 73) {
+    age.getMerked(age.remainingEarthLife);
+    $('#mercExp').html(`you would live ${age.remainingEarthLife} more years`);
+  } else {
+    age.getMerked(age.winningBy);
+    $('#mercExp').html(`you would live ${age.winningBy} more years`);
+  }
 }
 function venusInfo(age) {
-  age.getVenused(age.userAge);
-  $('#venus').toggle();
+  $('#venus').show();
   $('#mercury').hide();
   $('#jupiter').hide();
   $('#mars').hide();
+  age.getVenused(age.userAge);
   $('#venAge').html(age.newYears);
+  age.lifeLeftOnEarth();
+  if (age.userAge < 73) {
+    age.getVenused(age.remainingEarthLife);
+    $('#venExp').html(`you would live ${age.remainingEarthLife} more years`);
+  } else {
+    age.getVenused(age.winningBy);
+    $('#venExp').html(`you would live ${age.winningBy} more years`);
+  }
 }
 function marsInfo(age) {
-  age.getMarsed(age.userAge);
-  $('#mars').toggle();
+  $('#mars').show();
   $('#mercury').hide();
   $('#jupiter').hide();
   $('#venus').hide();
+  age.getMarsed(age.userAge);
   $('#marsAge').html(age.newYears);
+  age.lifeLeftOnEarth();
+  if (age.userAge < 73) {
+    age.getMarsed(age.remainingEarthLife);
+    $('#marsExp').html(`you would live ${age.remainingEarthLife} more years`);
+  } else {
+    age.getMarsed(age.winningBy);
+    $('#marsExp').html(`you would live ${age.winningBy} more years`);
+  }
 }
 function jupiterInfo(age) {
-  age.getJupt(age.userAge);
-  $('#jupiter').toggle();
+  $('#jupiter').show();
   $('#mercury').hide();
   $('#venus').hide();
   $('#mars').hide();
   $('#juptAge').html(age.newYears);
+  age.getJupt(age.userAge);
+  age.lifeLeftOnEarth();
+  if (age.userAge < 73) {
+    age.getJupt(age.remainingEarthLife);
+    $('#juptExp').html(`you would live ${age.remainingEarthLife} more years`);
+  } else {
+    age.getJupt(age.winningBy);
+    $('#juptExp').html(`you would live ${age.winningBy} more years`);
+  }
+}
+function mercuryExpectency() {
+  $('#mercExp').show();
+}
+function venusExpectency() {
+  $('#venExp').show();
+}
+function marsExpectency() {
+  $('#marsExp').show();
+}
+function jupiterExpectency() {
+  $('#juptExp').show();
+
 }
 $(document).ready(function () {
   $('#formy').submit(function (event) {
@@ -57,5 +102,19 @@ $(document).ready(function () {
     $('#jupiterButton').click(function () {
       jupiterInfo(usersAge);
     });
+
+    $('#mercExpButton').click(function () {
+      mercuryExpectency(usersAge);
+    });
+    $('#venExpButton').click(function () {
+      venusExpectency(usersAge);
+    });
+    $('#marsExpButton').click(function () {
+      marsExpectency(usersAge);
+    });
+    $('#juptExpButton').click(function () {
+      jupiterExpectency(usersAge);
+    });
+
   });
 });
